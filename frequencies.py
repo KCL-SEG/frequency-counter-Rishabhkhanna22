3009022f3956a18c9ea56f0b06a91033aca01dc8
Rishabhkhanna22 <147158567+Rishabhkhanna22@users.noreply.github.com>
2023-12-08 04:07:19 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if item not in frequencies:
+            frequencies[item] = 0
+        frequencies[item] += 1
     return frequencies
+
+print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
+print(frequencies([100, 'Hello', '100', '100', 100]))
